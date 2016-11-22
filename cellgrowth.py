f757bb63d0f1c71457d899639adbf4888b0be4f1
quendil1 <samuel.churlaud@cri-paris.org>
2016-11-22 13:04:13 +0100

    commit

--- cellgrowth.py
+++ cellgrowth.py
@@ -1,3 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy.array as arr
 import matplotlib.animation as an
+import numpy.random as rand
+
+n_loop = 100         # length of simulation
+n_cell = 1           # number of cells at the beginning
+divTime = 1          # division time of cells
+gridSize = 100       # size of grid side, square root of possible number of cells
+
+
+
+
+
+
+
+cell = arr([round(rand.random() * gridSize)])
+
+
+
+
+
+
+
+
+
+
