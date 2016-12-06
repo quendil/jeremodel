620c7a51e2d8eb596679b5298a637ead9fdf5771
quendil1 <samuel.churlaud@cri-paris.org>
2016-12-06 21:25:40 +0100

    commit

--- cellgrowth.py
+++ cellgrowth.py
@@ -1,24 +1,26 @@
+# written by Samuel Churlaud - quendil1
+# version 2.1
+# generate the growth of cells on a grid
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
 
 N = 100            # size of grid side
 n_loop = 200       # number of iterations
 state = [1, 0]     # possible starting states for any position
 prob = 0.001       # probability of having one cell in one position
-counter = []
-prog = 0
+counter = []       # counter for progress
 
 
-# populate grid with random on/off - more off than on
-grid = np.random.choice(state, N * N, p=[prob, 1 - prob]).reshape(N, N)
+grid = np.random.choice(state, N * N, p=[prob, 1 - prob]).reshape(N, N)  # generate grid and populate it
 
 
 def update(data):
     global grid
-    # copy grid since we require 8 neighbors for calculation
-    # and we go line by line
-    newGrid = grid.copy()
+    newGrid = grid.copy()  # copy grid
 
     for i in range(N - 1):
         # if not first line normal, if first line don't take into account the i-1 (first line minus one)
@@ -32,7 +34,6 @@ def update(data):
                             grid[i - 1, j - 1] + grid[i - 1, j + 1] +
                             grid[i + 1, j - 1] + grid[i + 1, j + 1]) / 255
 
-                    # apply Conway's rules
                     # if case not empty and not surrounded, try to fill on of surrounding, else skip
                     if (grid[i, j] == 0) or (surr == 8):
                         continue
@@ -81,7 +82,7 @@ def update(data):
         # exception for first i line
         else:
             for j in range(N - 1):
-                # if not first line normal, if first line don't take into account the j-1 (first line minus one)
+                # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
                 if j != 0:
                     # compute 8-neighbor sum and divide by 255 to know how many are full
                     surr = (grid[i, j - 1] + grid[i, j + 1] + grid[i + 1, j] +
@@ -136,9 +137,9 @@ def update(data):
 
         # exception for last i line
         for j in range(N - 1):
-            # if not first line normal, if first line don't take into account the j-1 (first line minus one)
+            # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
             if j != 0:
-                # compute 8-neighbor sum and divide by 255 to know how many are full
+                # compute 8-neighbour sum and divide by 255 to know how many are full
                 surr = (grid[i, j - 1] + grid[i, j + 1] + grid[i - 1, j] +
                         grid[i - 1, j - 1] + grid[i - 1, j + 1]) / 255
 
@@ -206,4 +207,3 @@ mat = ax.matshow(grid)
 ani = animation.FuncAnimation(fig, update, frames=n_loop, interval=100, blit=True)
 ani.save('animation.gif', writer='imagemagick', fps=10)
 # plt.show()
-ani.event_source.stop()
