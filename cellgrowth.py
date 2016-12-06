8f65bac32c437b908d25e5f6d8cb338067ca2cfc
quendil1 <samuel.churlaud@cri-paris.org>
2016-12-06 22:28:06 +0100

    commit

--- cellgrowth.py
+++ cellgrowth.py
@@ -1,38 +1,40 @@
+# coding = UTF-8
 # written by Samuel Churlaud - quendil1
-# version 2.1
-# generate the growth of cells on a grid
+# version 2.2
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import os.path
 
 
-N = 100            # size of grid side
-n_loop = 200       # number of iterations
-state = [1, 0]     # possible starting states for any position
-prob = 0.001       # probability of having one cell in one position
-counter = []       # counter for progress
+N = 50               # size of grid side
+n_loop = 100         # number of iterations
+state = [1, 0]       # possible starting state for any position
+prob = 0.001         # probability for a position to be a cell
+counter = []         # progress counter
 
+fileNumber = 1
+while os.path.exists('animation' + str(N) + '_' + str(n_loop) + '_' + str(fileNumber) + '.gif') is True:
+    fileNumber += 1
 
 grid = np.random.choice(state, N * N, p=[prob, 1 - prob]).reshape(N, N)  # generate grid and populate it
 
 
 def update(data):
-    global grid
-    newGrid = grid.copy()  # copy grid
 
     for i in range(N - 1):
-        # if not first line normal, if first line don't take into account the i-1 (first line minus one)
+        # if not first i line normal, if first i line don't take into account the i-1 (first line minus one)
         if i != 0:
             for j in range(N - 1):
-                # if not first line normal, if first line don't take into account the j-1 (first line minus one)
+                # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
                 if j != 0:
-                    # compute 8-neighbor sum and divide by 255 to know how many are full
+                    # compute 8-neighbour sum to know how many are full
                     surr = (grid[i, j - 1] + grid[i, j + 1] +
                             grid[i - 1, j] + grid[i + 1, j] +
                             grid[i - 1, j - 1] + grid[i - 1, j + 1] +
-                            grid[i + 1, j - 1] + grid[i + 1, j + 1]) / 255
+                            grid[i + 1, j - 1] + grid[i + 1, j + 1])
 
                     # if case not empty and not surrounded, try to fill on of surrounding, else skip
                     if (grid[i, j] == 0) or (surr == 8):
@@ -44,13 +46,13 @@ def update(data):
                             j -= 1
                             continue
                         else:
-                            newGrid[i + x, j + y] = 2
+                            grid[i + x, j + y] = 1
                             continue
 
                 # exception for first j line
                 else:
                     surr = (grid[i, j + 1] + grid[i - 1, j] + grid[i + 1, j] +
-                            grid[i - 1, j + 1] + grid[i + 1, j + 1]) / 255
+                            grid[i - 1, j + 1] + grid[i + 1, j + 1])
 
                     if (grid[i, j] == 0) or (surr == 5):
                         continue
@@ -61,34 +63,35 @@ def update(data):
                             j -= 1
                             continue
                         else:
-                            newGrid[i + x, j + y] = 1
+                            grid[i + x, j + y] = 1
 
             # exception for last j line
             j = N - 1
             surr = (grid[i, j - 1] + grid[i - 1, j] + grid[i + 1, j] +
-                    grid[i - 1, j - 1] + grid[i + 1, j - 1]) / 255
+                    grid[i - 1, j - 1] + grid[i + 1, j - 1])
+
             if (grid[i, j] == 0) or (surr == 5):
-                continue
+                pass
             else:
-                x = np.random.choice([-1, 0, 1])
-                y = np.random.choice([-1, 0])
-                if grid[i + x, j + y] == 1:
-                    j -= 1
-                    continue
-                else:
-                    newGrid[i + x, j + y] = 1
-                    continue
+                while j == N - 1:
+                    x = np.random.choice([-1, 0, 1])
+                    y = np.random.choice([-1, 0])
+                    if grid[i + x, j + y] == 1:
+                        pass
+                    else:
+                        grid[i + x, j + y] = 1
+                        j = 0
+                        pass
 
         # exception for first i line
         else:
             for j in range(N - 1):
                 # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
                 if j != 0:
-                    # compute 8-neighbor sum and divide by 255 to know how many are full
+                    # compute 8-neighbour sum to know how many are full
                     surr = (grid[i, j - 1] + grid[i, j + 1] + grid[i + 1, j] +
-                            grid[i + 1, j - 1] + grid[i + 1, j + 1]) / 255
+                            grid[i + 1, j - 1] + grid[i + 1, j + 1])
 
-                    # apply Conway's rules
                     # if case not empty and not surrounded, try to fill on of surrounding, else skip
                     if (grid[i, j] == 0) or (surr == 5):
                         continue
@@ -99,13 +102,13 @@ def update(data):
                             j -= 1
                             continue
                         else:
-                            newGrid[i + x, j + y] = 1
+                            grid[i + x, j + y] = 1
                             continue
 
                 # exception for first j line
                 else:
                     surr = (grid[i, j + 1] + grid[i + 1, j] +
-                            grid[i + 1, j + 1]) / 255
+                            grid[i + 1, j + 1])
 
                     if (grid[i, j] == 0) or (surr == 3):
                         continue
@@ -116,94 +119,97 @@ def update(data):
                             j -= 1
                             continue
                         else:
-                            newGrid[i + x, j + y] = 1
+                            grid[i + x, j + y] = 1
 
             # exception for last j line
             j = N - 1
             surr = (grid[i, j - 1] + grid[i + 1, j] +
-                    grid[i + 1, j - 1]) / 255
+                    grid[i + 1, j - 1])
 
             if (grid[i, j] == 0) or (surr == 3):
+                pass
+            else:
+                while j == N - 1:
+                    x = np.random.choice([0, 1])
+                    y = np.random.choice([-1, 0])
+                    if grid[i + x, j + y] == 1:
+                        pass
+                    else:
+                        grid[i + x, j + y] = 1
+                        j = 0
+                        pass
+
+    # exception for last i line
+    i = N - 1
+    for j in range(N - 1):
+        # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
+        if j != 0:
+            # compute 8-neighbour sum to know how many are full
+            surr = (grid[i, j - 1] + grid[i, j + 1] + grid[i - 1, j] +
+                    grid[i - 1, j - 1] + grid[i - 1, j + 1])
+
+            # if case not empty and not surrounded, try to fill on of surrounding, else skip
+            if (grid[i, j] == 0) or (surr == 5):
                 continue
             else:
-                x = np.random.choice([0, 1])
-                y = np.random.choice([-1, 0])
+                x = np.random.choice([-1, 0])
+                y = np.random.choice([-1, 0, 1])
                 if grid[i + x, j + y] == 1:
                     j -= 1
                     continue
                 else:
-                    newGrid[i + x, j + y] = 1
+                    grid[i + x, j + y] = 1
                     continue
 
-        # exception for last i line
-        for j in range(N - 1):
-            # if not first j line normal, if first j line don't take into account the j-1 (first line minus one)
-            if j != 0:
-                # compute 8-neighbour sum and divide by 255 to know how many are full
-                surr = (grid[i, j - 1] + grid[i, j + 1] + grid[i - 1, j] +
-                        grid[i - 1, j - 1] + grid[i - 1, j + 1]) / 255
-
-                # apply Conway's rules
-                # if case not empty and not surrounded, try to fill on of surrounding, else skip
-                if (grid[i, j] == 0) or (surr == 5):
-                    continue
-                else:
-                    x = np.random.choice([-1, 0])
-                    y = np.random.choice([-1, 0, 1])
-                    if grid[i + x, j + y] == 1:
-                        j -= 1
-                        continue
-                    else:
-                        newGrid[i + x, j + y] = 1
-                        continue
-
-            # exception for first j line
-            else:
-                surr = (grid[i, j + 1] + grid[i - 1, j] +
-                        grid[i - 1, j + 1]) / 255
-
-                if (grid[i, j] == 0) or (surr == 3):
-                        continue
-                else:
-                    x = np.random.choice([-1, 0])
-                    y = np.random.choice([0, 1])
-                    if grid[i + x, j + y] == 1:
-                        j -= 1
-                        continue
-                    else:
-                        newGrid[i + x, j + y] = 1
-
-        # exception for last j line
-        j = N - 1
-        surr = (grid[i, j - 1] + grid[i - 1, j] +
-                grid[i - 1, j - 1]) / 255
-
-        if (grid[i, j] == 0) or (surr == 3):
-            continue
+        # exception for first j line
         else:
+            surr = (grid[i, j + 1] + grid[i - 1, j] +
+                    grid[i - 1, j + 1])
+
+            if (grid[i, j] == 0) or (surr == 3):
+                    continue
+            else:
+                x = np.random.choice([-1, 0])
+                y = np.random.choice([0, 1])
+                if grid[i + x, j + y] == 1:
+                    j -= 1
+                    continue
+                else:
+                    grid[i + x, j + y] = 1
+
+    # exception for last j line
+    j = N - 1
+    surr = (grid[i, j - 1] + grid[i - 1, j] +
+            grid[i - 1, j - 1])
+
+    if (grid[i, j] == 0) or (surr == 3):
+        pass
+    else:
+        while j == N - 1:
             x = np.random.choice([-1, 0])
             y = np.random.choice([-1, 0])
             if grid[i + x, j + y] == 1:
-                j -= 1
-                continue
+                pass
             else:
-                newGrid[i + x, j + y] = 1
-                continue
+                grid[i + x, j + y] = 1
+                j = 0
+                pass
 
-    # update data
-    mat.set_data(newGrid)
-    grid = newGrid
+    # update counter
     if (len(counter) % 10) == 0:
-        print("avancement %.0f%%" % (100. * float(len(counter)) / float(n_loop)))
+        print("progress %.0f%%" % (100. * float(len(counter)) / float(n_loop)))
     else:
         pass
-    counter.append(1)
+    counter.append(0)
+
+    # update data
+    mat.set_data(grid)
     return [mat]
 
 
 # set up animation
 fig, ax = plt.subplots()
 mat = ax.matshow(grid)
-ani = animation.FuncAnimation(fig, update, frames=n_loop, interval=100, blit=True)
-ani.save('animation.gif', writer='imagemagick', fps=10)
+ani = animation.FuncAnimation(fig, update, frames=n_loop, interval=1, save_count=50, blit=True)
+ani.save('animation' + str(N) + '_' + str(n_loop) + '_' + str(fileNumber) + '.gif', writer='imagemagick', fps=10)
 # plt.show()
