3ea107b5775cfa643040d98d7b6cee54ed6f4ba7
quendil1 <samuel.churlaud@cri-paris.org>
2016-12-08 21:15:00 +0100

    commit

--- antibioresistance.py
+++ antibioresistance.py
@@ -1,9 +1,9 @@
 # coding = UTF-8
-# written by Samuel Churlaud - quendil1
+# written by Nikola Zarevski and Samuel Churlaud
 # distributed under license GPL-3.0
-# cells growing on a plate
+# cells growing on a plate with mutation of the value describing them (here antibioresistance)
 # for JereModel on github.com/quendil1/jeremodel
-# version 3.2
+# version 1.0
 
 
 import numpy as np
@@ -12,23 +12,28 @@ import matplotlib.animation as animation
 import os.path
 
 
-N = 400               # size of grid side
-n_loop = 200          # number of iterations
-state = [1, -30]      # possible starting state for any position
-prob = 0.001          # probability for a position to be a cell
-counter = []          # progress counter
+N = 200                          # size of grid side
+n_loop = 100                     # number of iterations
+emptyValue = -20                 # value for empty case
+deathValue = -10                 # value for case with dead cell in it
+state = [1, emptyValue]          # possible starting state for any position
+prob = 0.001                     # probability for a position to be a cell
+mutationRate = 3                 # mutation rate
+counter = []                     # progress counter
+diversity = 5                    # genetic diversity of the population, the bigger the more diverse (arbitrary unit)
+averageRes = 50                  # average resistance
+maxRes = 100                     # maximum resistance for a cell
 
-diversity = 5         # genetic diversity of the population, the bigger the more diverse (arbitrary unit)
-averageRes = 50       # average resistance
-
-
+# for gif generation, generate file name
 fileNumber = 1
 while os.path.exists('animation' + str(N) + '_' + str(n_loop) + '_' + str(fileNumber) + '.gif') is True:
     fileNumber += 1
 
 
 def cellgrowth(grid):
-    newGrid = grid
+    # generate growth of cells on the plate, with mutation
+    newGrid = grid    # copy grid
+
     for i in range(N - 2):
         i += 1
         for j in range(N - 2):
@@ -46,7 +51,13 @@ def cellgrowth(grid):
                     j -= 1
                     continue
                 else:
-                    newGrid[i + x, j + y] = grid[i, j]
+                    a = grid[i, j] + np.random.normal(0, mutationRate)     # change resistance value because of mutations
+                    if a < 0:
+                        newGrid[i + x, j + y] = 0
+                    if a > maxRes:
+                        newGrid[i + x, j + y] = maxRes
+                    else:
+                        newGrid[i + x, j + y] = a
                     continue
 
     # update counter
@@ -68,7 +79,7 @@ for i in range(N):
     for j in range(N):
         if grid[i, j] == 1:
             a = abs(np.random.normal(averageRes, diversity))
-            if (a > 100) or (a < 0):
+            if (a > maxRes) or (a < 0):
                 j -= 1
             else:
                 grid[i, j] = a
@@ -81,7 +92,8 @@ def update(data):
 # set up animation
 fig, ax = plt.subplots()
 mat = ax.matshow(grid)
-plt.colorbar(mat)
+cbar = fig.colorbar(mat, boundaries=[-20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+cbar.set_clim([-20, 100])
 ani = animation.FuncAnimation(fig, update, frames=n_loop, interval=1, save_count=50, blit=True)
-ani.save('animation' + str(N) + '_' + str(n_loop) + '_' + str(fileNumber) + '.gif', writer='imagemagick', fps=10)
-# plt.show()
+# ani.save('animation' + str(N) + '_' + str(n_loop) + '_' + str(fileNumber) + '.gif', writer='imagemagick', fps=10)
+plt.show()
